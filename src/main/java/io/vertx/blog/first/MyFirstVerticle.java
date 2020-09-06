72c17c2828d758ec03e1f87109a293db8d369b39
F6173079 Leandro dos Santos Coutinho <lescoutinhovr@gmail.com>
2020-09-06 18:08:29 -0300

    Add vertx-web dependency so we can use routing

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -2,17 +2,30 @@ package io.vertx.blog.first;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerResponse;
+
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MyFirstVerticle extends AbstractVerticle {
 	
 	@Override
 	public void start(Future<Void> fut) {
+		
+		Router router = Router.router(vertx);
+		
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response
+				.putHeader("content-type", "text/html")
+				.end("<h1>Hello from my first Vert.x 3 application</h1>");
+		});
+		
 		vertx
 			.createHttpServer()
-			.requestHandler(r -> {
-				r.response().end("<h1>Hello from my first " +
-					"Vert.x 3 application</h1>");
-			})
+			.requestHandler(router::accept)
 			.listen(
 				config().getInteger("http_port", 8080),
 				result -> {
@@ -24,4 +37,8 @@ public class MyFirstVerticle extends AbstractVerticle {
 				}
 			);
 	}
+	
+	/*private Route getHello(Handler<RoutingContext> requestHandler) {
+		return null;
+	}*/
 }
