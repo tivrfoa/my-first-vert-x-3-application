8cb01d421c0694a353ef75b6577470b46041066b
F6173079 Leandro dos Santos Coutinho <lescoutinhovr@gmail.com>
2020-09-07 21:55:14 -0300

    Add integration test: check we can retrieve an individual product

---
+++ src/test/java/io/vertx/blog/first/MyRestIT.java
@@ -0,0 +1,41 @@
+package io.vertx.blog.first;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MyRestIT {
+	
+	@BeforeClass
+	public static void configureRestAssured() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = Integer.getInteger("http_port", 8080);
+	}
+	
+	@AfterClass
+	public static void unconfigureRestAssured() {
+		RestAssured.reset();
+	}
+	
+	@Test
+	public void checkThatWeCanRetrieveIndividualProduct() {
+		// Get the list of bottles, ensure it's a success and extract the first id.
+		final int id = get("/api/whiskies").then()
+		  .assertThat()
+		  .statusCode(200)
+		  .extract()
+		  .jsonPath().getInt("find { it.name=='Bowmore 15 Years Laimrig' }.id");
+		// Now get the individual resource and check the content
+		get("/api/whiskies/" + id).then()
+		  .assertThat()
+		  .statusCode(200)
+		  .body("name", equalTo("Bowmore 15 Years Laimrig"))
+		  .body("origin", equalTo("Scotland, Islay"))
+		  .body("id", equalTo(id));
+	}
+}
