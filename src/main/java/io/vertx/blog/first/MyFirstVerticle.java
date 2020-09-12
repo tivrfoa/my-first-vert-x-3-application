1df334eb9e01c09b2f22be19364714fa6107e32a
F6173079 Leandro dos Santos Coutinho <lescoutinhovr@gmail.com>
2020-09-12 19:25:24 -0300

    bump vertx versions. Use Launcher instead of Starter Include database connection

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -4,35 +4,72 @@ import java.util.Map;
 import java.util.LinkedHashMap;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
-import io.vertx.ext.web.handler.BodyHandler;
-import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.sql.UpdateResult;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MyFirstVerticle extends AbstractVerticle {
 	
 	private Map<Integer, Whisky> products = new LinkedHashMap<>();
 	private int port;
+	private JDBCClient jdbc;
 	
-	private void createSomeData() {
+	/*private void createSomeData() {
 		Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
 		products.put(bowmore.getId(), bowmore);
 		Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
 		products.put(talisker.getId(), talisker);
-	}
+	}*/
 	
 	@Override
 	public void start(Future<Void> fut) {
 		
 		port = config().getInteger("http_port", 8080);
 		
-		createSomeData();
+		jdbc = JDBCClient.createShared(vertx, config(), "My-Whisky-Collection");
+
+		startBackend(
+			(connection) -> createSomeData(connection,
+				(nothing) -> startWebApp(
+					(http) -> completeStartup(http, fut)
+				), fut
+			), fut);
+	}
+	
+	private void startBackend(Handler<AsyncResult<SQLConnection>> next,
+			Future<Void> fut) {
+		jdbc.getConnection(ar -> {
+		  if (ar.failed()) {
+			fut.fail(ar.cause());
+		  } else {
+			next.handle(Future.succeededFuture(ar.result()));
+		  }
+		});
+	}
+	
+	private void completeStartup(AsyncResult<HttpServer> http, Future<Void> fut) {
+		if (http.succeeded()) {
+		  fut.complete();
+		} else {
+		  fut.fail(http.cause());
+		}
+	}	
+		
+	private void startWebApp(Handler<AsyncResult<HttpServer>> next) {
 		
 		Router router = Router.router(vertx);
 		
@@ -59,13 +96,7 @@ public class MyFirstVerticle extends AbstractVerticle {
 			.requestHandler(router::accept)
 			.listen(
 				port,
-				result -> {
-					if (result.succeeded()) {
-						fut.complete();
-					} else {
-						fut.fail(result.cause());
-					}
-				}
+				next::handle
 			);
 			
 		System.out.println("*************************************** " +
@@ -137,4 +168,106 @@ public class MyFirstVerticle extends AbstractVerticle {
 		}
 		routingContext.response().setStatusCode(204).end();
 	}
+	
+	@Override
+	  public void stop() throws Exception {
+		// Close the JDBC client.
+		jdbc.close();
+	  }
+	  
+  private void createSomeData(AsyncResult<SQLConnection> result, Handler<AsyncResult<Void>> next, Future<Void> fut) {
+	  
+	  Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+	products.put(bowmore.getId(), bowmore);
+	Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+	products.put(talisker.getId(), talisker);
+	  
+	  
+    if (result.failed()) {
+      fut.fail(result.cause());
+    } else {
+      SQLConnection connection = result.result();
+      connection.execute(
+          "CREATE TABLE IF NOT EXISTS Whisky (id INTEGER IDENTITY, name varchar(100), origin varchar" +
+              "(100))",
+          ar -> {
+            if (ar.failed()) {
+              fut.fail(ar.cause());
+              connection.close();
+              return;
+            }
+            connection.query("SELECT * FROM Whisky", select -> {
+              if (select.failed()) {
+                fut.fail(select.cause());
+                connection.close();
+                return;
+              }
+              if (select.result().getNumRows() == 0) {
+                insert(
+                    new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay"), connection,
+                    (v) -> insert(new Whisky("Talisker 57° North", "Scotland, Island"), connection,
+                        (r) -> {
+                          next.handle(Future.<Void>succeededFuture());
+                          connection.close();
+                        }));
+              } else {
+                next.handle(Future.<Void>succeededFuture());
+                connection.close();
+              }
+            });
+
+          });
+    }
+  }
+  
+  private void insert(Whisky whisky, SQLConnection connection, Handler<AsyncResult<Whisky>> next) {
+    String sql = "INSERT INTO Whisky (name, origin) VALUES ?, ?";
+    connection.updateWithParams(sql,
+        new JsonArray().add(whisky.getName()).add(whisky.getOrigin()),
+        (ar) -> {
+          if (ar.failed()) {
+            next.handle(Future.failedFuture(ar.cause()));
+            connection.close();
+            return;
+          }
+          UpdateResult result = ar.result();
+          // Build a new whisky instance with the generated id.
+          Whisky w = new Whisky(result.getKeys().getInteger(0), whisky.getName(), whisky.getOrigin());
+          next.handle(Future.succeededFuture(w));
+        });
+  }
+
+  private void select(String id, SQLConnection connection, Handler<AsyncResult<Whisky>> resultHandler) {
+    connection.queryWithParams("SELECT * FROM Whisky WHERE id=?", new JsonArray().add(id), ar -> {
+      if (ar.failed()) {
+        resultHandler.handle(Future.failedFuture("Whisky not found"));
+      } else {
+        if (ar.result().getNumRows() >= 1) {
+          resultHandler.handle(Future.succeededFuture(new Whisky(ar.result().getRows().get(0))));
+        } else {
+          resultHandler.handle(Future.failedFuture("Whisky not found"));
+        }
+      }
+    });
+  }
+
+  private void update(String id, JsonObject content, SQLConnection connection,
+                      Handler<AsyncResult<Whisky>> resultHandler) {
+    String sql = "UPDATE Whisky SET name=?, origin=? WHERE id=?";
+    connection.updateWithParams(sql,
+        new JsonArray().add(content.getString("name")).add(content.getString("origin")).add(id),
+        update -> {
+          if (update.failed()) {
+            resultHandler.handle(Future.failedFuture("Cannot update the whisky"));
+            return;
+          }
+          if (update.result().getUpdated() == 0) {
+            resultHandler.handle(Future.failedFuture("Whisky not found"));
+            return;
+          }
+          resultHandler.handle(
+              Future.succeededFuture(new Whisky(Integer.valueOf(id),
+                  content.getString("name"), content.getString("origin"))));
+        });
+  }
 }
